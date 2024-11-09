b838ef544739a3d8928f0d8cfcbbecd7f44b3455
ELVIS <141669940+elvispereira072@users.noreply.github.com>
2024-11-09 13:28:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('mobile-menu');
+    const navList = document.querySelector('.nav-list');
+
+    menuToggle.addEventListener('click', function() {
+        navList.classList.toggle('show');
+    });
+});
